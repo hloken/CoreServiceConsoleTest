c702e83b0b9537908f95c1e60bcca25de9379f7d
Hans Løken <hans.loken@nrk.no>
2018-06-27 09:22:09 +0200

    Add project files.

---
+++ CoreServiceConsoleTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreServiceConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
